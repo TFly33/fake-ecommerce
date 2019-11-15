11d28f5c189a0fc313c2af5ef9aa7fc34b934b89
Tommy Flynn <TFly.Flynn@gmail.com>
2019-11-15 18:29:35 -0500

    made the table. Couple of other small adds

---
+++ bamazon.sql
@@ -0,0 +1,38 @@
+-- Drops the programming_db if it already exists --
+DROP DATABASE IF EXISTS bamazon_db;
+-- Create a database called programming_db --
+CREATE DATABASE bamazon_db;
+
+-- Use programming db for the following statements --
+USE bamazon_db;
+
+CREATE TABLE products(
+  -- Create a numeric column called "id" which will automatically increment its default value as we create new rows. --
+item_id INT(20) AUTO_INCREMENT NOT NULL, 
+  -- Create a string column called "language" --
+product_name VARCHAR(100) NOT NULL,
+  -- Create an integer column called "rating" --
+department_name VARCHAR(100) NOT NULL,
+  -- Create an integer column called "rating" --
+price DECIMAL (10,5) NOT NULL,
+
+stock_quantity DECIMAL (10,5) NOT NULL,
+
+  -- Set the id as this table's primary key
+  PRIMARY KEY (item_id)
+);
+
+-- Create new example rows
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Rickey Henderson","modern", 50, 7), 
+("Ken Griffey Jr.","modern", 100, 6), 
+("Mickey Mantle","classic", 200, 3),
+("Babe Rush","antique", 500, 2),
+("Honus Wagner","antique", 10000, 1),
+("Greg Maddux","modern", 200, 82), 
+("Jay Buhner","modern", 30, 250), 
+("Shoeless Joe Jackson","antique", 1000, 4), 
+("Yogi Berra","classic", 150, 350), 
+("Mike Trout","active", 80, 500), 
+("Anthony Rendon","active", 25, 1000)
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,49 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
+ 
+// instantiate. Come back to this table. 
+// var table = new Table({
+//     head: ['TH 1 label', 'TH 2 label']
+//   , colWidths: [100, 200]
+// });
+ 
+// // table is an Array, so you can `push`, `unshift`, `splice` and friends
+// table.push(
+//     ['First value', 'Second value']
+//   , ['First value', 'Second value']
+// );
+ 
+// console.log(table.toString());
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Voltaire279G",
+    database: "bamazon_db"
+});
+
+
+// PUT SWITCH HERE. 
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+    
+});
+
+    function start() {
+        connection.query("SELECT * FROM products", function(err, res) {
+          if (err) throw err;
+          console.log(res);
+          connection.end();
+        });
+      }
